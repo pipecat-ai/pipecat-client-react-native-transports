dd7879e8e811acad5395ad599a613caca2b520cc
Filipi Fuchter <filipi87@gmail.com>
2025-10-31 10:28:30 -0300

    Creating dev script

---
+++ transports/daily/example/dev_scripts/clear_and_rebuild.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# clear
+rm -rf ./ios/
+rm -rf ./android/
+rm -rf node_modules/
+# Install dependencies
+yarn install
+# Before a native app can be compiled, the native source code must be generated.
+npx expo prebuild
