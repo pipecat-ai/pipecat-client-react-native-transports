410b0de1c1e9c512ef69e4a8bc0f575206310d08
Filipi Fuchter <filipi87@gmail.com>
2025-11-06 09:41:09 -0300

    Exposing the PipecatClientVideoView.

--- transports/smallwebrtc/src/index.tsx
+++ transports/smallwebrtc/src/index.tsx
@@ -3,9 +3,11 @@ import {
   SmallWebRTCTransportConstructorOptions,
 } from './transport';
 import { MediaManager } from './media-manager/mediaManager';
+import PipecatClientVideoView from './view/PipecatClientVideoView';
 
 export {
   RNSmallWebRTCTransport,
   SmallWebRTCTransportConstructorOptions,
   MediaManager,
+  PipecatClientVideoView,
 };
